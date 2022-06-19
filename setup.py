cd324d08f2b5200ad7538ef741fdf96b96a48bf3
unknown <tlawogud12@naver.com>
2022-06-20 02:40:55 +0900

    version 1.2.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ='jhfunnycode',
+    version = '1.2.0',
+    description = 'life is fun',
+    author = 'i_enable',
+    author_email = None,
+    url = None,
+    install_requires = [],
+    packages=find_packages(),
+)
